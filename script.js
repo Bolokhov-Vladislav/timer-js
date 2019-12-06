4b94dab768ce6c88b618f2dfca3995b35e7923c3
Bolokhov Vladyslav <40151122+Bolokhov-Vladislav@users.noreply.github.com>
2019-12-06 18:25:34 +0200

    clean code

---
+++ script.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', function() {
+    let deadline = '2019-12-15';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date());
+
+        let seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/1000/60/60) % 24),
+            days = Math.floor((t/(1000*60*60*24)));
+           
+            return {
+                'total' : t,
+                'days' : days,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+            
+    }
+
+    function setClock (id, endtime ) {
+        
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+        
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            (t.seconds < 10 ) ? seconds.textContent = '0' + t.seconds : seconds.textContent = t.seconds;
+            (t.minutes < 10 ) ? minutes.textContent = '0' + t.minutes : minutes.textContent = t.minutes;
+            (t.hours < 10) ? hours.textContent = '0' + t.hours : hours.textContent = t.hours;
+            ((t.days % 10) === 1) ? days.textContent = t.days + ' день' : days.textContent = t.days + ' дней';
+            ((t.days % 10) >= 2 && (t.days % 10) <= 4) ? days.textContent = t.days + ' дня' : days.textContent = t.days + ' дней';
+            
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.textContent = '0';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+    setClock('timer', deadline);
+});
